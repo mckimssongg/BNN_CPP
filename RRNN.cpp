0a113c8bd79e745762a4342ac09b6bf086b5feb9
mckimssongg <soymckimsson286@gmail.com>
2025-02-10 22:45:53 -0600

    modificar RRNN.cpp para ajustar la configuración de la red neuronal y evaluar el riesgo de enfermedad en pacientes

--- RRNN.cpp
+++ RRNN.cpp
@@ -4,58 +4,26 @@
 int main(){
 
     srand(time(NULL));
+    rand();
 
+    // double edad, nivel_de_colesterol, presion_arterial;
     std::vector<size_t> configuracion;
-    configuracion.push_back(2);
-    configuracion.push_back(2);
+    configuracion.push_back(3);
+    configuracion.push_back(5);
     configuracion.push_back(1);
-    PerceptronMulticapa p(configuracion);
 
-    std::vector<std::vector<std::vector<double> > > pesos_iniciales;
+    PerceptronMulticapa red_neuronal(configuracion);
+    // Entrenamiento
+    std::vector<double> datos_paciente;
 
-    std::vector<std::vector<double> > capa_oculta;
-    std::vector<double> neurona1;
-    neurona1.push_back(-10);
-    neurona1.push_back(-10);
-    neurona1.push_back(15);
+    datos_paciente.push_back(0.55);
+    datos_paciente.push_back(0.7);
+    datos_paciente.push_back(0.65);
 
-    std::vector<double> neurona2;
-    neurona2.push_back(15);
-    neurona2.push_back(15);
-    neurona2.push_back(-10);
-
-    capa_oculta.push_back(neurona1);
-    capa_oculta.push_back(neurona2);
-
-    std::vector<std::vector<double> > capa_salida;
-    std::vector<double> neurona_salida;
-    neurona_salida.push_back(10);
-    neurona_salida.push_back(10);
-    neurona_salida.push_back(-15);
-    capa_salida.push_back(neurona_salida);
-
-    pesos_iniciales.push_back(capa_oculta);
-    pesos_iniciales.push_back(capa_salida);
-
-    std::vector<double> combinacion1;
-    combinacion1.push_back(0);
-    combinacion1.push_back(0);
-    std::cout << "0 XOR 0 : " << p.ejecutar(combinacion1)[0] << std::endl;
-
-    std::vector<double> combinacion2;
-    combinacion2.push_back(0);
-    combinacion2.push_back(1);
-    std::cout << "0 XOR 1 : " << p.ejecutar(combinacion2)[0] << std::endl;
-
-    std::vector<double> combinacion3;
-    combinacion3.push_back(1);
-    combinacion3.push_back(0);
-    std::cout << "1 XOR 0 : " << p.ejecutar(combinacion3)[0] << std::endl;
-
-    std::vector<double> combinacion4;
-    combinacion4.push_back(1);
-    combinacion4.push_back(1);
-    std::cout << "1 XOR 1 : " << p.ejecutar(combinacion4)[0] << std::endl;
+    std::vector<double> salida = red_neuronal.ejecutar(datos_paciente);
     
+    if(salida[0] > 0.5) std::cout << "Alto riesgo de enfermedad. Realice más pruebas." << std::endl;
+    else std::cout << "Bajo riesgo de enfermedad. Continúe manteniendo un estilo de vida saludable." << std::endl;
+
     return 0;
 }
