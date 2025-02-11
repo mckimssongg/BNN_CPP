9276586dad6b67c4b98f222d18033e92029762d3
mckimssongg <soymckimsson286@gmail.com>
2025-02-10 21:40:57 -0600

    agregar configuración de asociación de archivos y mejorar la implementación de Perceptron y PerceptronMulticapa

--- PerceptronMulticapa.cpp
+++ PerceptronMulticapa.cpp
@@ -1,3 +1,41 @@
 #include "PerceptronMulticapa.h"
 
+double frand(){
+    return (2.0*(double)rand() / RAND_MAX) - 1.0;
+}
 
+Perceptron::Perceptron(size_t numero_de_entradas, double bias){
+    this->bias = bias;
+    pesos.resize(numero_de_entradas+1);
+    generate(pesos.begin(), pesos.end(), frand);
+}
+
+void Perceptron::establecer_pesos(std::vector<double> pesos_iniciales){
+    pesos = pesos_iniciales;
+}
+
+double Perceptron::ejecutar(std::vector<double> datos_de_entrada){
+    datos_de_entrada.push_back(bias);
+    double suma_ponderada = std::inner_product(datos_de_entrada.begin(),datos_de_entrada.end(),pesos.begin(),(double)0.0);
+    return sigmoide(suma_ponderada);
+}
+
+double Perceptron::sigmoide(double x){
+    return 1.0/(1.0 + exp(-x));
+}
+
+PerceptronMulticapa::PerceptronMulticapa(std::vector<size_t> capas, double bias){
+    this->capas = capas;
+    this->bias = bias;
+
+    for(size_t i=0; i < capas.size(); i++){
+        valores.push_back(std::vector<double>(capas[i],0.0));
+        red.push_back(std::vector<Perceptron>());
+
+        if(i>0){
+            for(size_t j = 0; j<capas[i]; j++){
+                red[i].push_back(Perceptron(capas[i-1], bias));
+            }
+        }
+    }    
+}
--- PerceptronMulticapa.h
+++ PerceptronMulticapa.h
@@ -8,14 +8,21 @@
 class Perceptron
 { 
     public:
+        Perceptron::Perceptron(size_t numero_de_entradas, double bias = 1.0);
         double ejecutar(std::vector<double> datos_de_entrada);
         void establecer_pesos(std::vector<double> pesos_iniciales);
         // funcion_de_activacion
         double sigmoide(double x);
+        std::vector<double> pesos;
         double bias;
 };
 
 class PerceptronMulticapa
 {
     public:
+        PerceptronMulticapa::PerceptronMulticapa(std::vector<size_t> capas, double bias);
+        std::vector<std::vector<Perceptron> > red;
+        std::vector<size_t> capas;   
+        std::vector<std::vector<double> > valores;
+        double bias;
 };
