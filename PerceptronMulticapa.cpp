988a2fff308a03bacfb80f502afb4d97be0fa64a
mckimssongg <soymckimsson286@gmail.com>
2025-02-10 22:07:24 -0600

    agregar método retro en PerceptronMulticapa para calcular el retropropagación y ajustar pesos

--- PerceptronMulticapa.cpp
+++ PerceptronMulticapa.cpp
@@ -24,9 +24,10 @@ double Perceptron::sigmoide(double x){
     return 1.0/(1.0 + exp(-x));
 }
 
-PerceptronMulticapa::PerceptronMulticapa(std::vector<size_t> capas, double bias){
+PerceptronMulticapa::PerceptronMulticapa(std::vector<size_t> capas, double bias, double eta){
     this->capas = capas;
     this->bias = bias;
+    this->eta = eta;
 
     for(size_t i=0; i < capas.size(); i++){
         valores.push_back(std::vector<double>(capas[i],0.0));
@@ -58,4 +59,47 @@ std::vector<double> PerceptronMulticapa::ejecutar(std::vector<double> datos_de_e
         }
     }
     return valores.back();
+}
+
+double PerceptronMulticapa::retro(std::vector<double> x, std::vector<double> y){
+
+    // Paso 1: alimentar la red
+    std::vector<double> salidas = ejecutar(x);
+
+    // Paso 2: calcular el error cuadrático medio
+    double error_cuadratico_medio = 0.0;
+    std::vector<double> error;
+
+    for(size_t i=0; i<y.size();i++){
+        error.push_back(y[i] - salidas[i]);
+        error_cuadratico_medio += error[i] * error[i];
+    }
+
+    error_cuadratico_medio /= capas.back();
+
+    // Paso 3: calcular los términos de error de salida
+    for(size_t i = 0; i < salidas.size(); i++){
+        d.back()[i] = salidas[i] * (1-salidas[i]) * (error[i]);
+    }
+    // Paso 4: calcular el término de error de cada unidad en cada capa
+    for(size_t i= red.size()-2; i>0;i--){
+        for(size_t h=0;h<red[i].size();h++){
+            double retro_error = 0.0;
+            for(size_t k=0; k<capas[i+1];k++){
+                retro_error += red[i+1][k].pesos[h]*valores[i+1][k];
+            }
+            d[i][h] = valores[i][h]*(1-valores[i][h])*retro_error;
+        }
+    }
+    
+    // Pasos 5 y 6: calcular las deltas y actualizar los pesos
+    for (size_t i = 1; i < red.size(); i++)
+        for (size_t j = 0; j < capas[i]; j++)
+            for (size_t k = 0; k < capas[i-1]+1; k++){
+                double delta;
+                if (k == capas[i-1]) delta = eta * d[i][j] * bias;
+                else delta = eta * d[i][j] * valores[i-1][k];
+                
+                red[i][j].pesos[k] += delta;
+            }
 }
--- PerceptronMulticapa.h
+++ PerceptronMulticapa.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 #include <numeric>
 #include <algorithm>
@@ -6,22 +7,25 @@
 #include <cmath>
 
 class Perceptron{
-    public:
-        Perceptron(size_t numero_de_entradas, double bias=1.0);
-        double ejecutar(std::vector<double> datos_de_entrada);
-        void establecer_pesos(std::vector<double> pesos_iniciales);
-        double sigmoide(double x);
-        std::vector<double> pesos;
-        double bias;
-    };
-    
-    class PerceptronMulticapa{
-    public:
-        PerceptronMulticapa(std::vector<size_t> capas, double bias);
-        void establecer_pesos(std::vector<std::vector<std::vector<double> > > pesos_iniciales);
-        std::vector<double> ejecutar(std::vector<double> datos_de_entrada);
-        std::vector<std::vector<Perceptron> > red;
-        std::vector<size_t> capas;   
-        std::vector<std::vector<double> > valores;
-        double bias;
-    };
+public:
+    Perceptron(size_t numero_de_entradas, double bias=1.0);
+    double ejecutar(std::vector<double> datos_de_entrada);
+    void establecer_pesos(std::vector<double> pesos_iniciales);
+    double sigmoide(double x);
+    std::vector<double> pesos;
+    double bias;
+};
+
+class PerceptronMulticapa{
+public:
+    PerceptronMulticapa(std::vector<size_t> capas, double bias=1.0, double eta=0.01);
+    void establecer_pesos(std::vector<std::vector<std::vector<double> > > pesos_iniciales);
+    std::vector<double> ejecutar(std::vector<double> datos_de_entrada);
+    double retro(std::vector<double> x, std::vector<double> y);
+    std::vector<std::vector<Perceptron> > red;
+    std::vector<size_t> capas;
+    std::vector<std::vector<double> > valores;
+    std::vector<std::vector<double> > d;
+    double bias;
+    double eta;
+};
