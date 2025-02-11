3e4939a40d267c7d2c5152fa131c5d1db6489f41
mckimssongg <soymckimsson286@gmail.com>
2025-02-10 22:43:49 -0600

    modificar RRNN.cpp para implementar una red neuronal multicapa y ajustar la salida de las pruebas XOR

--- RRNN.cpp
+++ RRNN.cpp
@@ -4,36 +4,58 @@
 int main(){
 
     srand(time(NULL));
-    rand();
 
-    Perceptron p(2);
+    std::vector<size_t> configuracion;
+    configuracion.push_back(2);
+    configuracion.push_back(2);
+    configuracion.push_back(1);
+    PerceptronMulticapa p(configuracion);
 
-    std::vector<double> pesos_iniciales;
-    pesos_iniciales.push_back(10);
-    pesos_iniciales.push_back(10);
-    pesos_iniciales.push_back(-5);
-    
-    p.establecer_pesos(pesos_iniciales);
+    std::vector<std::vector<std::vector<double> > > pesos_iniciales;
+
+    std::vector<std::vector<double> > capa_oculta;
+    std::vector<double> neurona1;
+    neurona1.push_back(-10);
+    neurona1.push_back(-10);
+    neurona1.push_back(15);
+
+    std::vector<double> neurona2;
+    neurona2.push_back(15);
+    neurona2.push_back(15);
+    neurona2.push_back(-10);
+
+    capa_oculta.push_back(neurona1);
+    capa_oculta.push_back(neurona2);
+
+    std::vector<std::vector<double> > capa_salida;
+    std::vector<double> neurona_salida;
+    neurona_salida.push_back(10);
+    neurona_salida.push_back(10);
+    neurona_salida.push_back(-15);
+    capa_salida.push_back(neurona_salida);
+
+    pesos_iniciales.push_back(capa_oculta);
+    pesos_iniciales.push_back(capa_salida);
 
     std::vector<double> combinacion1;
     combinacion1.push_back(0);
     combinacion1.push_back(0);
-    std::cout << "0 || 0 : " << p.ejecutar(combinacion1) << std::endl;
+    std::cout << "0 XOR 0 : " << p.ejecutar(combinacion1)[0] << std::endl;
 
     std::vector<double> combinacion2;
     combinacion2.push_back(0);
     combinacion2.push_back(1);
-    std::cout << "0 || 1 : " << p.ejecutar(combinacion2) << std::endl;
+    std::cout << "0 XOR 1 : " << p.ejecutar(combinacion2)[0] << std::endl;
 
     std::vector<double> combinacion3;
     combinacion3.push_back(1);
     combinacion3.push_back(0);
-    std::cout << "1 || 0 : " << p.ejecutar(combinacion3) << std::endl;
+    std::cout << "1 XOR 0 : " << p.ejecutar(combinacion3)[0] << std::endl;
 
     std::vector<double> combinacion4;
     combinacion4.push_back(1);
     combinacion4.push_back(1);
-    std::cout << "1 || 1 : " << p.ejecutar(combinacion4) << std::endl;
+    std::cout << "1 XOR 1 : " << p.ejecutar(combinacion4)[0] << std::endl;
     
     return 0;
 }
