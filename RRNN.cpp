f6c71472489867d8fc871d8468be5aef32e31769
mckimssongg <soymckimsson286@gmail.com>
2025-02-10 22:43:30 -0600

    modificar mensajes de salida en RRNN.cpp para utilizar '||' en lugar de 'AND'

--- RRNN.cpp
+++ RRNN.cpp
@@ -11,29 +11,29 @@ int main(){
     std::vector<double> pesos_iniciales;
     pesos_iniciales.push_back(10);
     pesos_iniciales.push_back(10);
-    pesos_iniciales.push_back(-15);
+    pesos_iniciales.push_back(-5);
     
     p.establecer_pesos(pesos_iniciales);
 
     std::vector<double> combinacion1;
     combinacion1.push_back(0);
     combinacion1.push_back(0);
-    std::cout << "0 AND 0 : " << p.ejecutar(combinacion1) << std::endl;
+    std::cout << "0 || 0 : " << p.ejecutar(combinacion1) << std::endl;
 
     std::vector<double> combinacion2;
     combinacion2.push_back(0);
     combinacion2.push_back(1);
-    std::cout << "0 AND 1 : " << p.ejecutar(combinacion2) << std::endl;
+    std::cout << "0 || 1 : " << p.ejecutar(combinacion2) << std::endl;
 
     std::vector<double> combinacion3;
     combinacion3.push_back(1);
     combinacion3.push_back(0);
-    std::cout << "1 AND 0 : " << p.ejecutar(combinacion3) << std::endl;
+    std::cout << "1 || 0 : " << p.ejecutar(combinacion3) << std::endl;
 
     std::vector<double> combinacion4;
     combinacion4.push_back(1);
     combinacion4.push_back(1);
-    std::cout << "1 AND 1 : " << p.ejecutar(combinacion4) << std::endl;
+    std::cout << "1 || 1 : " << p.ejecutar(combinacion4) << std::endl;
     
     return 0;
 }
